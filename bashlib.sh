011f7ef51013c9ca218e8d45f17ee25962241650
edlittle <ed.little@outlook.com>
2016-05-23 23:01:09 +1000

    Terminal notifier updated
    Changes made to look for terminal notifier rather than gems folder

--- bashlib.sh
+++ bashlib.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Shell Library - bashlib 
+# Shell Library - bashlib
 # Used to store regular and repeated commands that can be imported
-# into other bash scripts by way of the `source` command. 
-# 
+# into other bash scripts by way of the `source` command.
+#
 # usage:
 # inside a script you wish to make use of this library...
 # `source bashlib -args`
-# 
+#
 # arguments:
 # 	-v : 	Verbose enabled, when using log, will also print to console.
 # 	-n : 	Invoking script name, this will be used to identify your script
@@ -15,7 +15,7 @@
 # 	-l :	Set log path to new value
 # 	-p :  Enables the use of terminal-notifier for push notifications
 # 	-o :  Use Pushover to send notifications
-# 
+#
 
 
 
@@ -38,7 +38,7 @@ BASELOGPATH=~/Library/Logs/bashlib
 LOGPATH=$BASELOGPATH
 FILENAME="bashlib"
 VERBOSE=0;					# If verbose calls return to the terminal or just are entered in the log
-TN=0 								# Default value, set when terminal-notifier is found
+TN=0 								# Default value, set when terminal-notifier is found - version checked: 1.6.3
 TNOVERRIDE=1				# Use terminal-notifier if present
 TRIGGER=0						# Forces scrip to exit if the name is not present
 PUSHO=0
@@ -61,7 +61,7 @@ if [[ ! -d $LOGPATH ]]; then
 	mkdir $LOGPATH
 fi
 
-if [[ -d /Library/Ruby/Gems/2.0.0 ]]; then
+if [[ -d /Library/Ruby/Gems/2.0.0/gems/terminal-notifier-1.6.3 ]]; then
 	TN=1
 fi
 
@@ -85,7 +85,7 @@ fi
 
 
 
-# Core Functions 
+# Core Functions
 # ==================================================================== #
 # Main Functions of Bashlib
 # ==================================================================== #
@@ -107,7 +107,7 @@ log_file() {
     printf "\033[0;33m"
     printf "$FILENAME: logged file"
     echo "\033[0m"
-  fi 
+  fi
   touch $LOGPATH/$FILENAME.log
   while read p; do
     echo "$(date +'%d %b %T') $FILENAME: $p" >> $LOGPATH/$FILENAME.log
@@ -156,7 +156,7 @@ pushover() {
 
 
 
-if [[ ! -d /Library/Ruby/Gems/2.0.0 ]]; then
+if [[ ! -d /Library/Ruby/Gems/2.0.0/gems/terminal-notifier-1.6.3 ]]; then
 	internalecho "terminal-notifier is not found on this system.\nIt can be installed using sudo gem install terminal-notifier"
 fi
 
@@ -175,14 +175,14 @@ for (( i = 0; i < $ARGSLENGTH; i++ )); do
 			FILENAME=$OPTARG
 			TRIGGER=1
 			;;
-		l) 
-			internalecho "updated log path to: $OPTARG" 
+		l)
+			internalecho "updated log path to: $OPTARG"
 			LOGPATH=$OPTARG
 			if [[ ! -d $LOGPATH ]]; then
 				mkdir $LOGPATH
 			fi
 			;;
-		p) 
+		p)
 			if [[ $TN == 1 ]]; then
 				internalecho "Push notifications have been enabled"
 				TNOVERRIDE=1
